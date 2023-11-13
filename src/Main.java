99ee38b761d6860649ad283d20748cea8e2fb7b8
yuval21104 <127964202+yuvalsw21104@users.noreply.github.com>
2023-11-13 19:48:52 +0200

    added a new line of output

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args){
         System.out.println("my first Java project");
+        System.out.println("I have just changed the code!");
     }
 }
